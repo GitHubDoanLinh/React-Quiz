3235c1865bc1c6fa6baffe5f28ab7da3e8ee7472
Doan Huu Linh <doanlinh1510@gmail.com>
2024-08-31 18:37:19 +0700

    commit2 the first useReducer - count state

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
-  // This mutates the date object.
   const date = new Date("august 30 2024");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
@@ -25,7 +31,6 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
     setStep(1);
   };
 
