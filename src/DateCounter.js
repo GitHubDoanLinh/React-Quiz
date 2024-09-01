208a6bbd3556dc616cb0caf6d25ecb3cda93ad00
Doan Huu Linh <doanlinh1510@gmail.com>
2024-09-01 16:48:21 +0700

    commit 3 useReducer - Managing Related Pieces of State

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,15 +1,33 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - 1 };
+
+    case "inc":
+      return { ...state, count: state.count + 1 };
+
+    case "setCount":
+      return { ...state, count: action.payload };
+
+    case "setStep":
+      return { ...state, step: action.payload };
+
+    case "reset":
+      return initialState;
+
+    default:
+      throw new Error("Unknow action");
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   const date = new Date("august 30 2024");
   date.setDate(date.getDate() + count);
@@ -27,11 +45,11 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
